65e72fb9649055c635555efe9bd1b22528d1c41e
Sumit <sk.shankar@bradyplc.com>
2016-08-30 16:21:27 +0530

    Test Website - Github
    Test Website - Github

---
+++ GitHubTutorial/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubTutorial.Startup))]
+namespace GitHubTutorial
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
